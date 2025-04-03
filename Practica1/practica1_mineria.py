b125f9b9665bdccec46426bbb6bb63e18d274a9e
Ariadne Romero <88512668+AriadneRomero@users.noreply.github.com>
2025-04-02 18:09:47 -0600

    Create practica #1
    Se descarga el dataset que se encuentra en Kaggle

---
+++ Practica1/practica1_mineria.py
@@ -0,0 +1,15 @@
+# Ariadne Selena Romero Rivero #1957540 Practica#1 Mineria de datos
+# Libreria
+# !pip install opendatasets
+import opendatasets as od
+
+dataset_link = "https://www.kaggle.com/datasets/gregorut/videogamesales"
+od.download(dataset_link)
+
+import os
+os.chdir("./videogamesales")
+os.listdir()
+
+import pandas as pd
+data = pd.read_csv("vgsales.csv")
+print(data)

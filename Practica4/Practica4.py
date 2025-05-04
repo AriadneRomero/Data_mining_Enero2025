8ec1f8fb0d4e9a1996f6b5bfeb019bc13c43a9bc
Ariadne Romero <88512668+AriadneRomero@users.noreply.github.com>
2025-05-03 20:25:17 -0600

    Practicas 2, 3, 4

---
+++ Practica4/Practica4.py
@@ -0,0 +1,35 @@
+import pandas as pd
+import numpy as np
+
+import scipy.stats as stats
+import statsmodels.api as sm
+from statsmodels.formula.api import ols
+from statsmodels.stats.multicomp import pairwise_tukeyhsd
+
+# Lectura de datos
+data = pd.read_csv("videogamesales/videogamesales_clean.csv")
+
+def analizar_diferencias_genero(data):
+    # Modelo ANOVA
+    modl = ols('Global_Sales ~ Genre', data=data).fit()
+    anova_df = sm.stats.anova_lm(modl, typ=2)
+
+    valor_p = anova_df["PR(>F)"].iloc[0]
+
+    if valor_p < 0.05:
+        print("✔️ Hay diferencias significativas entre géneros")
+        print(anova_df)
+
+        # Prueba post-hoc de Tukey
+        tukey = pairwise_tukeyhsd(endog=data["Global_Sales"],
+                                    groups=data["Genre"],
+                                    alpha=0.05)
+
+        print("\n🔍 Resultados de la prueba de Tukey:")
+        print(tukey)
+
+    else:
+        print("❌ No hay diferencias significativas entre géneros")
+        
+        
+analizar_diferencias_genero(data)
